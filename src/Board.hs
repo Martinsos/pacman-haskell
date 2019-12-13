78d47d4ff7c0c3b536eb27283de89cd0ed6b65b1
Martin Sosic <sosic.martin@gmail.com>
2019-12-13 18:01:40 +0100

    Made nice refactoring of Board.hs, but now I need to propagate the changes.

--- src/Board.hs
+++ src/Board.hs
@@ -1,49 +1,75 @@
 module Board
-    ( Board
-    , Field(..)
-    , Position(..)
-    , newPosition
+    ( Board(..)
+    , Pacman(..)
+    , Object(..)
+    , Position
     , Direction(..)
-    , getWidth
-    , getHeight
-    , fromList
-    , toList
+    , fromAscii
     ) where
 
+import Control.Exception (assert)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
 data Board = Board
-    { getRows :: [[Field]]
-    }
+    { getBoardWidth :: Int
+    , getBoardHeight :: Int
+    , getBoardPacman :: Pacman
+    , getBoardObjects :: Map Position Object
+    } deriving (Show)
 
-data Field = Food | Wall | Empty deriving (Show, Eq)
+data Pacman = Pacman
+    { getPacmanDirection :: Direction
+    , getPacmanPosition :: Position
+    } deriving (Show)
 
-data Position = Position
-    { getX :: Int
-    , getY :: Int
-    }
+data Object = Food | Wall deriving (Show, Eq)
 
-newPosition :: Int -> Int -> Position
-newPosition x y = Position { getX = x, getY = y }
+type Position = (Int, Int) -- (x, y) where top left corner of board is (0, 0) and bottom right is (width, height).
 
 data Direction = North | East | South | West deriving (Eq, Show, Ord)
 
-getWidth :: Board -> Int
-getWidth board = length $ (getRows board) !! 0
-
-getHeight :: Board -> Int
-getHeight board = length $ getRows board
-
--- TODO: add method that constructs board from string, from ascii characters, so it is easier to construct it (more readable).
-
-fromList :: [[Field]] -> Board
-fromList rows
+fromAscii :: [String] -> Board
+fromAscii rows
     | not areRowsValid = error "Rows are invalid!"
-    | otherwise = Board { getRows = rows }
+    | otherwise = Board
+                  { getBoardHeight = height
+                  , getBoardWidth = width
+                  , getBoardPacman = pacman
+                  , getBoardObjects = objects
+                  }
   where
+    areRowsValid = (height > 0) && (width > 0) && (all ((== width) . length) rows)
+    height = length rows
     width = length (rows !! 0)
-    areRowsValid = (length rows > 0)
-        && (width > 0)
-        && (all (\row -> length row == width) rows)
 
-toList :: Board -> [[Field]]
-toList board = getRows board
+    cells :: [(Position, Char)]
+    cells = concatMap (\(x, row) -> map (\(y, char) -> (((x, y), char))) (zip [0..] row)) (zip [0..] rows)
+
+    pacmans :: [Pacman]
+    pacmans = [p | Just p <- map (\(position, char) -> pacmanFromAscii position char) cells]
+    pacman = assert (length pacmans == 1) $ head pacmans
+
+    objectCells :: [(Position, Object)]
+    objectCells = [ (position, object)
+                  | (position, Just object) <- map (\(position, char) -> (position, objectFromAscii char)) cells
+                  ]
+    objects = Map.fromList objectCells
+
+objectFromAscii :: Char -> Maybe Object
+objectFromAscii '.' = Just Food
+objectFromAscii '#' = Just Wall
+objectFromAscii _ = Nothing
+
+pacmanFromAscii :: Position -> Char -> Maybe Pacman
+pacmanFromAscii position char = if (char `elem` ("><^v" :: String))
+                                then Just (Pacman
+                                           { getPacmanPosition = position
+                                           , getPacmanDirection = directionFromAscii char})
+                                else Nothing
+    where
+      directionFromAscii '<' = East
+      directionFromAscii '>' = West
+      directionFromAscii '^' = South
+      directionFromAscii 'v' = North
+      directionFromAscii _ = error "Not a valid pacman character!"
